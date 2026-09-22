dfc9cd07981c80090c66ab70e864b0a3affe03ec
andersonluiznegocios-code <andersonluiz.negocios@gmail.com>
2026-09-22 16:42:07 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // PREENCHA com os dados do SEU projeto Supabase
 // Supabase > Project Settings > API
 // =========================================================
-const SUPABASE_URL = "COLE_AQUI_A_URL_DO_SEU_PROJETO_SUPABASE";
-const SUPABASE_ANON_KEY = "COLE_AQUI_A_ANON_KEY_DO_SEU_PROJETO_SUPABASE";
+const SUPABASE_URL = "https://bewhtwmoqvkanwapvhlz.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJld2h0d21vcXZrYW53YXB2aGx6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAxMDMzMzMsImV4cCI6MjEwNTY3OTMzM30.NEd5yl5CoPU4pi2qI6L5xr-qX5Y2ea_WMtm_lyIJHMU";
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
